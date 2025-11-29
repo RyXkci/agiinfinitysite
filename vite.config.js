946396edca93ff19136ca0db2aa2d41a2590e1ad
Ricardo <ricardobozzi23@gmail.com>
2025-11-29 16:53:35 +0100

    favicon

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+
+export default defineConfig({
+  plugins: [
+    vitePluginFaviconsInject('./public/images/agi.png')
+  ]
+});
+
+
+
+
+
